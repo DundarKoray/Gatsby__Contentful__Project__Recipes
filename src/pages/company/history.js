04d95fb54ca546fd0dccd568c321128649dd03c8
Koray Dundar <koraydundar_40@hotmail.com>
2022-03-25 13:13:34 +0200

    completed navbar

---
+++ src/pages/company/history.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+import Navbar from "../../components/Navbar"
+
+const history = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello from History Page</h1>
+    </div>
+  )
+}
+
+export default history
