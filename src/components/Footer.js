ef42c7375ef5ef58a4e61a70ef2abe50e59fc92b
Koray Dundar <koraydundar_40@hotmail.com>
2022-03-30 13:25:44 +0300

    completed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,14 @@ import React from "react"
 import { Link } from "gatsby"
 
 const Footer = () => {
-  return <footer>Footer</footer>
+  return (
+    <footer class="page-footer">
+      <p>
+        &copy; {new Date().getFullYear()} <span>SimplyRecipes</span>. Built with{" "}
+        <a href="https://www.gatsbyjs.com/">Gatsby</a>
+      </p>
+    </footer>
+  )
 }
 
 export default Footer
