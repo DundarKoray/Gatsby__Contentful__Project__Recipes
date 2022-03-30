b451fe50c38d34da54b8f3e95b7660a17e297037
Koray Dundar <koraydundar_40@hotmail.com>
2022-03-30 13:31:27 +0300

    completed error page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,16 @@
 import React from "react"
-import { Link } from "gatsby"
+import Layout from "./Layout"
 
 const Footer = () => {
   return (
-    <footer class="page-footer">
-      <p>
-        &copy; {new Date().getFullYear()} <span>SimplyRecipes</span>. Built with{" "}
-        <a href="https://www.gatsbyjs.com/">Gatsby</a>
-      </p>
-    </footer>
+    <Layout>
+      <footer class="page-footer">
+        <p>
+          &copy; {new Date().getFullYear()} <span>What we eat ?</span>. Built
+          with <a href="https://www.gatsbyjs.com/">Gatsby</a>
+        </p>
+      </footer>
+    </Layout>
   )
 }
 
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,14 @@
 import React from "react"
 
 const Error = () => {
-  return <div>404</div>
+  return (
+    <main class="error-page">
+      <section>
+        <h1>404</h1>
+        <h2>Page not found</h2>
+      </section>
+    </main>
+  )
 }
 
 export default Error
