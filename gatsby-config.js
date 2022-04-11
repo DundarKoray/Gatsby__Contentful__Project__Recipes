412f9fc0b6f0e1dea54c08db25a417c941464d27
Koray Dundar <koraydundar_40@hotmail.com>
2022-04-11 13:25:57 +0300

    site meta query

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,16 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "Nice and clean recipes site",
+    author: "@dundar_k",
+    person: { name: "Koray", age: 33 },
+    complexData: [
+      { name: "Koray", age: 33 },
+      { name: "Emma", age: 2 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
