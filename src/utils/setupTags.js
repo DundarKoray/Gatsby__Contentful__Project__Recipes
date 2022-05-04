9a48625392a000ee38b99cd3be2e5d1ee9229780
Koray Dundar <koraydundar_40@hotmail.com>
2022-05-04 14:04:17 +0300

    completed helper function

--- src/utils/setupTags.js
+++ src/utils/setupTags.js
@@ -1,6 +1,23 @@
 const setupTags = recipes => {
   console.log("Hello from utils")
-  return recipes
+  const allTags = {}
+  recipes.forEach(recipe => {
+    recipe.content.tags.forEach(tag => {
+      //   console.log(tag)
+      if (allTags[tag]) {
+        allTags[tag] = allTags[tag] + 1
+      } else {
+        allTags[tag] = 1
+      }
+    })
+  })
+  const newTags = Object.entries(allTags).sort((a, b) => {
+    const [firstTag] = a
+    const [secontTag] = b
+    return firstTag.localeCompare(secontTag)
+  })
+  //   console.log(allTags)
+  return newTags
 }
 
 export default setupTags
